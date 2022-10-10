5ed49973ae77d93da55d20e8bf4a1d872f759826
Lukas Foidl <wi20b044@technikum-wien.at>
2022-10-10 19:21:40 +0200

    initial project setup incl. Hello World controller and tests

---
+++ backend/src/main/java/at/barniverse/backend/barniverse_backend/BarniverseBackendApplication.java
@@ -0,0 +1,13 @@
+package at.barniverse.backend.barniverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarniverseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarniverseBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/at/barniverse/backend/barniverse_backend/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package at.barniverse.backend.barniverse_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
